c5451147e56730b9ec35e259c1402d980c69e4a0
Kobe Friswell <s3783258@student.rmit.edu.au>
2020-04-28 14:32:57 +1000

    Finished LinkedList and Nodes

---
+++ LinkedList.cpp
@@ -0,0 +1,63 @@
+#include "LinkedList.h"
+
+LinkedList::LinkedList(){
+    head = nullptr;
+    last = nullptr;
+    listSize = 0;
+}
+
+LinkedList::~LinkedList(){
+    clear();
+}
+      
+int LinkedList::size(){
+    //Return the int listSize as it keeps track of the size 
+    //when adding and clearing
+    return listSize;
+}
+
+void LinkedList::clear(){
+    Node* curr = head;
+    Node* prev;
+    head = nullptr;
+
+    while (curr != nullptr){
+        prev = curr;
+        curr = curr->next;
+
+        delete prev;
+    }
+
+    //Reset List Size
+    listSize = 0;
+}
+
+int LinkedList::get(int i){
+    Node* current = head;
+
+    while (i !=0){
+        i--;
+        current = current->next;
+    }
+    return current->data;
+}
+
+void LinkedList::addFront(int data){
+    Node* newNode = new Node(data,head);
+
+    //Check to see if new list with no nodes, meaning it will
+    //be first and last
+    if (head == nullptr){
+        last = newNode;
+    }
+
+    head = newNode;
+    listSize++;
+}
+
+void LinkedList::addBack(int data){
+    Node* newNode = new Node(data, nullptr);
+    last->next = newNode;
+    last = new Node(data, nullptr);
+    listSize++;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 //main file
 #include "Menu.h"
+#include "LinkedList.h"
 
 int main(){
+    //LinkedList Tests
+    LinkedList ll;
+    ll.addFront(1);
+    ll.addFront(4);
+    ll.addFront(3);
+    ll.addBack(7);
+
+    //Print Linked List
+    std::cout << "List Size: " << ll.size() << std::endl;
+    for (int i = 0; i < ll.size(); i++){
+        int num = ll.get(i);
+        std::cout << num << ", ";
+    }
+    std::cout << std::endl;
+
+
+    //Run Menu
     Menu* menu = new Menu();
     menu->mainMenu();
+
 
     return EXIT_SUCCESS;
 }
