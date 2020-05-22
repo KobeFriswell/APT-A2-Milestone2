89eb841adbc9b73c9be96df24afa09ca6b2c6d6f
jacky chuong s3780272 <s3780272@student.rmit.edu.au>
2020-05-22 20:50:33 +1000

    check

--- Mosaics.cpp
+++ Mosaics.cpp
@@ -1,78 +1,61 @@
-//Mosacis class
+#ifndef GAME_H
+#define GAME_H
 
-#include "Mosaics.h"
+#include "LinkedList.h"
+#include "Player.h"
+#include "Board.h"
+#include "Factories.h"
+#include "Bag.h"
+#include "readWrite.h"
+
+#include <string>
+#include <vector>
+
+#define NUM_FACTORIES 5
+
+class Game{
+public:
+    Game(std::string player1, std::string player2);
+
+    void addToPlayers(Player* player);
+
+    void addToLid(char tile);
+    std::string lidToString();
+
+    void addToCenter(char tile);
+    std::string centerToString();
+
+    bool playerTurn(int factoryChoice, char tile, int patternLine);
+    void finishRound();
+
+    void addToTileBag(char tile);
+
+    int getNumPlayers();
+    Player* getPlayer(int index);
+
+    Factories* getFactory(int index);
+
+    void saveGame(std::string fileName);
+
+    void setGameStatus(bool status);
+    void setCurrentPlayer(int current);
+    void setNumberPlayers();
+
+private:
+    std::vector<char> lid;
+    std::vector<char> center;
+    std::vector<Factories*> factories;
+    Bag tileBag;
     
+    //Cant do 2D array with 2 different types
+    std::vector<Player*> players;
+    std::vector<Board*> boards;
 
-Mosaics::Mosaics(Player* player){
-    this->player = player;
-    this->pointBoard = new char*[BOARD_SIZE];
-    this->turnBoard = new char*[BOARD_SIZE];
+    int numPlayers;
 
-    for(int x =0; x < 5; x++)
-    {
-        this->turnBoard[x] = new char[x+1];
-        this->pointBoard[x] = new char[BOARD_SIZE];
-        this->pointCalculator[x] = new char[BOARD_SIZE];
-    }
+    bool gameStatus;
+    int currentPlayer;
+    int numberPlayers;
+};
 
-    
-}
-Mosaics::~Mosaics(){
-player->~Player();
-    for(int x =0; x < BOARD_SIZE; x++)
-    {
-        delete this->pointBoard[x];
-        delete this->turnBoard[x];
-    }
-    for(int x =0; x < FLOOR_LINE_SIZE;x++)
-    {
-        delete broken[x];
-    }
-}
-
-
-bool Mosaics::PositionTiles(int row, int number, char tiles){
-        row -= 1;
-    
-    if(tiles == CURRENTPLAYERTURN)
-    {
-        this->holdfirst = true;
-        this->broken[this->brokenScore] = new char(tiles);
-        this->brokenScore++;
-        return true;
-    }
-
-}
-void Mosaics::setPlayer(Player* player){
-        this->player = player;
-
-}
-
-Player* Mosaics::getPlayer()
-{
-    return this->player;
-}
-
-void Mosaics::DisplayMosaics(){}
-void Mosaics::CheckTurn(){}
-void Mosaics::setTurn(bool turn){
-         this->currentTurn = turn;
-
-}
-
-
-void Mosaics::setBroken(char* tile, int area){}
-void Mosaics::setFirst(bool first){
-        this->holdfirst = first;
-
-}
-bool Mosaics::winCheck(){
-        
-}
-bool Mosaics::WhosFirst(){
-        return this->currentTurn;
-}
-bool Mosaics::whosTurn(){
-        return this->currentTurn;
-
-}
+#endif
