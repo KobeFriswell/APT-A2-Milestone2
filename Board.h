2a8800de0470728745739ebfea544115d43c9ba7
jacky chuong s3780272 <s3780272@student.rmit.edu.au>
2020-05-23 23:55:28 +1000

    adding scoring

--- Board.h
+++ Board.h
@@ -4,6 +4,10 @@
 #include <string>
 #include <iostream>
 
+#define tempLineX 0
+#define tempLineY 0
+#include "Player.h"
+
 #define GRID_SIZE 5
 #define PENALTY_LENGTH 7
 
@@ -32,10 +36,22 @@ public:
     void checkPatternLines();
     void clearPatternLine(int line);
 
+    int roundTally();
+
+    bool noAdjacentTile(int x, int y);
+
+    int endGameScoreTally();
+
+    bool verticalLineFull(int i);
+    bool horizontalLineFull(int i);
+
+
+
     std::string getFloorLine();
 
     char dropTile(char tile);
     void addFloorLine(char tile);
+    void clearFloorLine();
 
     std::vector<int> getNumToLid();
     std::vector<char> getTileToLid();
@@ -69,6 +85,14 @@ private:
     const int weightings[7] = {-1,-1,-2,-2,-2,-3,-3};
     char floorLine[FLOOR_LINE_SIZE] = {'-'};
     
+    //tracks playerscore
+    Player* player;
+    //int scorepoints
+    std::vector<int> checkX;
+    std::vector<int> checkY;
+    int brokenpoints = 0;
+    Board* board;
+
     //int scoreTrack;
     std::vector<int> numToLid;
     std::vector<char> tileToLid;
